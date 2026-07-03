4daaf2b70475fa1082f49f514346c0143cdc48b5
Elloani Ross A. Pitogo <pelloani@gmail.com>
2026-07-03 21:59:09 +0800

    feat(apps/web): add ffinancial overview summary cards in home page

--- apps/web/src/app/(main)/page.tsx
+++ apps/web/src/app/(main)/page.tsx
@@ -16,7 +16,21 @@ export default async function HomePage({
 
   return (
     <div>
-      <h1 className="text-grey-900 text-preset-1 font-bold">Overview</h1>
+      <h1 className="text-grey-900 text-preset-1 mb-8 font-bold">Overview</h1>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <div className="bg-grey-900 flex-1 rounded-lg p-6 text-white">
+          <p className="text-preset-4 mb-3">Current Balance</p>
+          <p className="text-preset-1 font-bold">$4,836.00</p>
+        </div>
+        <div className="text-grey-500 flex-1 rounded-lg bg-white p-6">
+          <p className="text-preset-4 mb-3">Income</p>
+          <p className="text-preset-1 text-grey-900 font-bold">$$3,814.25</p>
+        </div>
+        <div className="text-grey-500 flex-1 rounded-lg bg-white p-6">
+          <p className="text-preset-4 mb-3">Expenses</p>
+          <p className="text-preset-1 text-grey-900 font-bold">$1,700.50</p>
+        </div>
+      </div>
     </div>
   );
 }
